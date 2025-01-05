5ebd4569003f19f5e8a6e72e3505b62fde5eee23
MuntadherAteeq <mun.ateeq@outlook.com>
2025-01-05 10:30:45 +0300

    add toggle functionality and enhance Counter component in NewTab

--- src/NewTab/index.tsx
+++ src/NewTab/index.tsx
@@ -1,23 +1,46 @@
-import { createSignal } from "solid-js";
+import { createSignal, Show } from "solid-js";
 import { render } from "solid-js/web";
 import "virtual:uno.css";
-
-const [count, setCount] = createSignal(0);
 
 render(() => <NewTab />, document.getElementById("root")!);
 
 function NewTab() {
+  const [toggle, setToggle] = createSignal(false);
+  const counter = <Counter />;
   return (
-    <div class="p-4 bg-gray-100">
-      <h1 class="text-2xl font-bold text-red-500">Hello, SolidJS!</h1>
-      <p class="mt-2 p-4">You clicked {count()} times</p>
-      <div class=" i-tabler-apple-filled text-5xl text-red-4" />
-      <i class="i-mdi-home"></i>
-      <button
-        class="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
-        onClick={() => setCount(count() + 1)}
-      >
-        Click me
+    <div>
+      <button onClick={() => setToggle(!toggle())}>
+        {toggle() ? "Hide" : "Show"}
+      </button>
+      <div>
+        <div>
+          <div>
+            <Show when={!toggle()}>{counter}</Show>
+          </div>
+        </div>
+      </div>
+      <h1>
+        <div>
+          Solid js <i class="i-devicon-solidjs w-l h-l"></i>
+        </div>
+        <div>
+          Svelte <i class="i-devicon-svelte w-l h-l"></i>
+        </div>
+        <div>
+          React <i class="i-devicon-react w-l h-l"></i>
+        </div>
+      </h1>
+      <Show when={toggle()}>{counter}</Show>
+    </div>
+  );
+}
+
+function Counter(props: any) {
+  const [count, setCount] = createSignal(0);
+  return (
+    <div>
+      <button onClick={() => setCount(count() + 1)}>
+        Click me : {count()}
       </button>
     </div>
   );
