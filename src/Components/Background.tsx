1a51af1c058b23e4652c83e00bd4c181f2ae2d19
MuntadherAteeq <mun.ateeq@outlook.com>
2025-01-14 16:30:12 +0300

    add Background component and implement video wallpaper; refactor NewTab to use App component

---
+++ src/Components/Background.tsx
@@ -0,0 +1,75 @@
+import { createSignal, onMount, Show } from "solid-js";
+import wallpaper from "../../public/assets/10070-fhd.mp4";
+
+
+
+export default function Background(props: any) {
+  return (
+    <div class="pos-relative bg-gray-8 w-screen h-screen overflow-hidden">
+      <VideoWallpaper src={wallpaper} />
+    </div>
+  );
+}
+
+export const VideoWallpaper = (props: any) => {
+  const [show, setShow] = createSignal(false);
+
+  const handlePlay = (
+    e: Event & {
+      currentTarget: HTMLVideoElement;
+    }
+  ) => {
+    if (
+      e.currentTarget.currentTime >= Math.ceil(e.currentTarget.duration / 2)
+    ) {
+      setShow(true);
+    }
+  };
+  const HandleVideoLoaded = (
+    e: Event & {
+      currentTarget: HTMLVideoElement;
+    }
+  ) => {
+    e.currentTarget.style.animation = `fadeLoop ${e.currentTarget.duration}s infinite`;
+  };
+
+  const HandleVideoTimeUpdate = (
+    e: Event & {
+      currentTarget: HTMLVideoElement;
+    }
+  ) => {
+    let Video = e.currentTarget;
+    if (Video.currentTime >= Math.ceil(Video.duration / 2)) {
+      Video.style.display = "block";
+    } else {
+      Video.style.display = "hidden";
+    }
+  };
+  return (
+    <div class="pos-absolute grid place-items-center relative h-64 w-full h-full">
+      <video
+        autoplay
+        loop
+        muted
+        controls={false}
+        src={props.src}
+        disablepictureinpicture
+        class="col-start-1 row-start-1 w-full h-full object-cover"
+        onTimeUpdate={handlePlay}
+      />
+      <Show when={show()}>
+        <video
+          autoplay
+          loop
+          muted
+          controls={false}
+          src={props.src}
+          disablepictureinpicture
+          class=" col-start-1 row-start-1 w-full h-full  object-cover"
+          onloadedmetadata={HandleVideoLoaded}
+          onTimeUpdate={HandleVideoTimeUpdate}
+        />
+      </Show>
+    </div>
+  );
+};
--- src/NewTab/index.tsx
+++ src/NewTab/index.tsx
@@ -1,47 +1,10 @@
-import { createSignal, Show } from "solid-js";
+import Background from "@/Components/Background";
 import { render } from "solid-js/web";
 import "virtual:uno.css";
+import "@/global.css";
 
-render(() => <NewTab />, document.getElementById("root")!);
+render(() => <App />, document.getElementById("root")!);
 
-function NewTab() {
-  const [toggle, setToggle] = createSignal(false);
-  const counter = <Counter />;
-  return (
-    <div>
-      <button onClick={() => setToggle(!toggle())}>
-        {toggle() ? "Hide" : "Show"}
-      </button>
-      <div>
-        <div>
-          <div>
-            <Show when={!toggle()}>{counter}</Show>
-          </div>
-        </div>
-      </div>
-      <h1>
-        <div>
-          Solid js <i class="i-devicon-solidjs w-l h-l"></i>
-        </div>
-        <div>
-          Svelte <i class="i-devicon-svelte w-l h-l"></i>
-        </div>
-        <div>
-          React <i class="i-devicon-react w-l h-l"></i>
-        </div>
-      </h1>
-      <Show when={toggle()}>{counter}</Show>
-    </div>
-  );
-}
-
-function Counter() {
-  const [count, setCount] = createSignal(0);
-  return (
-    <div>
-      <button onClick={() => setCount(count() + 1)}>
-        Click me : {count()}
-      </button>
-    </div>
-  );
+function App() {
+  return <Background></Background>;
 }
